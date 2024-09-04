3dc00187c72a051ce0cb3d2fdd24bf99ff17daa6
Ord1nI <ordinandrej@gmail.com>
2024-09-04 16:47:47 +0300

    add check for zero devision

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 int main() {
     int num;
+    int num1 = 0 << 1;
     std::cout << "Hello world\n";
     std::cin >> num;
+    if (num == 0) {
+        std::cout << "Cant devide by zero";
+    } else {
+        std::cout << num1/num;
+    }
 }
